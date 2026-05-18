34bb4e49ff81294a9c818a167724bad75344bbc2
Caden Kranz <for.his.glory.dev@gmail.com>
2026-05-18 05:42:32 -0500

    Initial Expo app setup

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from "react-native";
 
 export default function App() {
+  console.log("App is rendering");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "white", justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 20 }}>Hello Caden!
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
