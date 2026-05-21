019b13b7ed105d5f6f6fb87cc2037edb35c55333
Caden Kranz <for.his.glory.dev@gmail.com>
2026-05-21 05:13:55 -0500

    Created timer logic, created basic UI for testing.

--- App.js
+++ App.js
@@ -1,12 +1,72 @@
-import { View, Text } from "react-native";
+// ----- Main Screen of the App ----- //
+import { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet } from "react-native";
 
 export default function App() {
-  console.log("App is rendering");
+  // ----- TIMER STATE ----- //
+  const [ timeLeft, setTimeLeft ] = useState( 10 );
+  const [ isRunning, setIsRunning ] = useState( false );
 
+  // ----- TIMER LOGIC ----- //
+  useEffect( () => {
+    let interval;
+
+    if ( isRunning && timeLeft > 0 ) {
+      interval = setInterval( () => {
+        setTimeLeft( ( previousTime ) => previousTime - 1 );
+      }, 1000 );
+    }
+    if ( timeLeft === 0 ) {
+      setIsRunning( false );
+    }
+    return () => clearInterval( interval );
+  }, [ isRunning, timeLeft ] );
+
+  // ----- RESET FUNCTION ----- //
+  const resetTimer = () => {
+    setIsRunning( false );
+    setTimeLeft( 10 );
+  };
+
+  // ----- UI ----- //
   return (
-    <View style={{ flex: 1, backgroundColor: "white", justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 20 }}>Hello Caden!
+    <View style={ styles.container }>
+      <Text style={ styles.timerText }>
+        { timeLeft }
       </Text>
+
+      <View style={ styles.buttonContainer }>
+        <Button
+          title="Start"
+          onPress={ () => setIsRunning( true ) }
+        />
+
+        <Button
+          title="Reset"
+          onPress={ resetTimer }
+        />
+      </View>
+
     </View>
   );
 }
+
+// ----- STYLES ----- //
+const styles = StyleSheet.create( {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+  timerText: {
+    fontSize: 72,
+    marginBottom: 40,
+  },
+
+  buttonContainer: {
+    width: 200,
+    gap: 20,
+  },
+} );
