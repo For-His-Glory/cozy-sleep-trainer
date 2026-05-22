3c5a58850cedad8296dedb03e2890fec1ea54df9
Caden Kranz <for.his.glory.dev@gmail.com>
2026-05-22 10:05:04 -0500

    Add alert logic and haptic/text feedback.

--- App.js
+++ App.js
@@ -1,32 +1,49 @@
 // ----- Main Screen of the App ----- //
 import { useState, useEffect } from 'react';
 import { View, Text, Button, StyleSheet } from "react-native";
+import * as Haptics from 'expo-haptics';
 
 export default function App() {
   // ----- TIMER STATE ----- //
   const [ timeLeft, setTimeLeft ] = useState( 10 );
   const [ isRunning, setIsRunning ] = useState( false );
+  const [ isDone, setIsDone ] = useState( false );
+
+  // ----- START TIMER ----- //
+  const startTimer = () => {
+    setIsRunning( true );
+    setIsDone( false );
+    setTimeLeft( 10 );
+  };
+
+  // ----- RESET FUNCTION ----- //
+  const resetTimer = () => {
+    setIsRunning( false );
+    setIsDone( false );
+    setTimeLeft( 10 );
+  };
 
   // ----- TIMER LOGIC ----- //
   useEffect( () => {
-    let interval;
+    let interval = null;
 
     if ( isRunning && timeLeft > 0 ) {
       interval = setInterval( () => {
         setTimeLeft( ( previousTime ) => previousTime - 1 );
       }, 1000 );
     }
-    if ( timeLeft === 0 ) {
+    if ( timeLeft === 0 && isRunning ) {
       setIsRunning( false );
+      setIsDone( true );
+
+      // ----- HAPTIC STATUS ----- //
+      Haptics.notificationAsync( 
+        Haptics.NotificationFeedbackType.Success
+      );
     }
+
     return () => clearInterval( interval );
   }, [ isRunning, timeLeft ] );
-
-  // ----- RESET FUNCTION ----- //
-  const resetTimer = () => {
-    setIsRunning( false );
-    setTimeLeft( 10 );
-  };
 
   // ----- UI ----- //
   return (
@@ -38,7 +55,7 @@ export default function App() {
       <View style={ styles.buttonContainer }>
         <Button
           title="Start"
-          onPress={ () => setIsRunning( true ) }
+          onPress={ startTimer }
         />
 
         <Button
@@ -47,6 +64,11 @@ export default function App() {
         />
       </View>
 
+      { isDone && ( 
+        <Text style={ { marginTop: 30, fontSize: 16 } }>
+          Session Complete!  Now Sleep :)
+        </Text>
+      ) }
     </View>
   );
 }
